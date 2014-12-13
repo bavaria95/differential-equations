01fded1b44d2163595264fce1ceb4eb4fe1c7d10
bavaria95 <bavaria95@gmail.com>
2014-12-13 16:08:58 +0100

    basic transformations

--- FDM.py
+++ FDM.py
@@ -1,21 +1,29 @@
 def shift(a):
-    return a[-1:] + a[:-1]
+    return a[-1 :] + a[: -1]
 
 A = []
 n = 4
-n -= 1
 
-a = [1] + [0 for i in range(n)]
+a = [1] + [0 for i in range(n - 1)]
 A.append(a)
 
-a = [1, -2, 1] + [0 for i in range(n - 2)]
+a = [1, -2, 1] + [0 for i in range(n - 3)]
 
-for i in range(n - 1):
+for i in range(n - 2):
     A.append(a)
     a = shift(a)
 
-a = [0 for i in range(n - 1)] + [-1, 1]
+a = [0 for i in range(n - 2)] + [-1, 1]
 A.append(a)
 
+A[0] = [float(k/A[0][0]) for k in A[0]]
+
+
+for i in range(1, n ):
+	A[i] = [A[i][j] - float(A[i  - 1][j]*A[i][i - 1]) for j in range(n)]
+	d = float(A[i][i])
+	A[i] = [k/d for k in A[i]]
+
+A = [[round(e, 3) for e in l] for l in A]
 
 print A
