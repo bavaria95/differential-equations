ed5b255a2e6f604eb3d69291599ea60ce9f2f79b
bavaria95 <bavaria95@gmail.com>
2014-12-13 17:12:02 +0100

    also generates right vector

--- FDM.py
+++ FDM.py
@@ -1,30 +1,36 @@
-def shift(a):
+'''
+	Heat flux model with finite difference method
+	'''
+
+def shift(a):					 
     return a[-1 :] + a[: -1]
 
 A = []
 n = 4
 
-a = [1] + [0 for i in range(n - 1)]
+a = [1] + [0]*(n - 1)
 A.append(a)
 
-a = [1, -2, 1] + [0 for i in range(n - 3)]
+a = [1, -2, 1] + [0]*(n - 3)
 
 for i in range(n - 2):
     A.append(a)
     a = shift(a)
 
-a = [0 for i in range(n - 2)] + [-1, 1]
+a = [0]*(n - 2) + [-1, 1]
 A.append(a)
 
-A[0] = [float(k/A[0][0]) for k in A[0]]
+H = [0]*(n - 1) + [1]
 
 
-for i in range(1, n ):
+'''
+	Gaussian elimination
+	'''
+
+for i in range(n ):
 	A[i] = [A[i][j] - float(A[i  - 1][j]*A[i][i - 1]) for j in range(n)]
+	H[i] -= H[i - 1]*A[i][i - 1]
 	d = float(A[i][i])
 	A[i] = [k/d for k in A[i]]
+	H[i] /= d
 
-A = [[round(e, 3) for e in l] for l in A]
-
-print A
-print d**-1
